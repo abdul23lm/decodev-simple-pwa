c26ad7c18e57359cd207d1a3c4aa896e91a69ff8
abdul23lm <abdul23lm@gmail.com>
2020-08-27 03:51:59 +0700

    Start catching

---
+++ sw.js
@@ -0,0 +1,12 @@
+var CACHE_NAME = "decodev-cache-v1";
+var urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
