6a6e62552b1f3e9ad07458a8eebbf504848510a7
abdul23lm <abdul23lm@gmail.com>
2020-08-27 04:46:18 +0700

    Add offline pages and styling index

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = "decodev-cache-v2";
-var urlsToCache = ["/", "/index.html", "/page.html"];
+var CACHE_NAME = "decodev-cache-v1";
+var urlsToCache = ["/", "/index.html", "/offline.html"];
 
 self.addEventListener("install", function (event) {
   // Perform install steps
@@ -13,42 +13,55 @@ self.addEventListener("install", function (event) {
 
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request).then(function (response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== "basic") {
+    caches
+      .match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
           return response;
         }
 
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        var responseToCache = response.clone();
+        return fetch(event.request).then(function (response) {
+          // Check if we received a valid response
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
+          }
 
-        caches.open(CACHE_NAME).then(function (cache) {
-          cache.put(event.request, responseToCache);
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
         });
-
-        return response;
-      });
-    })
+      })
+      .catch(function () {
+        // If both fail, show a generic fallback:
+        return caches.match("/offline.html");
+        // However, in reality you'd have many different
+        // fallbacks, depending on URL & headers.
+        // Eg, a fallback silhouette image for avatars.
+      })
   );
 });
 
 self.addEventListener("activate", function (event) {
-  var cacheAllowlist = CACHE_NAME;
+  var cacheWhitelist = CACHE_NAME;
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames.map(function (cacheName) {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
