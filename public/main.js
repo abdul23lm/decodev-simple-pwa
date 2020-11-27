cb709d1d355f34b69f12332adc1b12f25179d5a2
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-11-27 23:23:51 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -3,10 +3,7 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js").then(
       function (registration) {
         // Registration was successful
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
+
       },
       function (err) {
         // registration failed :(
